12403748672f7870a3ae04394cb710dc885c1518
Cabomaldade <soutom@yahoo.com.br>
2021-06-22 22:41:47 -0300

    finalizando aula 2 - criação e uso da orm com as migrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata";
 import express from "express";
+
+import "./database";
 
 const app = express();
 
 app.listen(3000, () => console.log("Server is running"));
-
-app.get("/test", (request, response) => {
-  return response.send("olá NWS");
-})
-
-app.post("/test-post", (resquest, response) => {
-  return response.send("olá NWS response post");
-})
